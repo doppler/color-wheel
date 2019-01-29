2a62346cfd4671cbd88e31484d6ab24c37a8ec79
David Rose <doppler@gmail.com>
2019-01-29 18:33:59 -0500

    Updates

---
+++ precache-manifest.a181df30e578efc3e05f5690066d93ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4eb14d0a1ed76c19df94",
+    "url": "/color-wheel/static/css/main.d2bf9b77.chunk.css"
+  },
+  {
+    "revision": "4eb14d0a1ed76c19df94",
+    "url": "/color-wheel/static/js/main.4eb14d0a.chunk.js"
+  },
+  {
+    "revision": "1215d76f22d5e49a20fa",
+    "url": "/color-wheel/static/js/1.1215d76f.chunk.js"
+  },
+  {
+    "revision": "fb7e50700f4868581ba1",
+    "url": "/color-wheel/static/js/runtime~main.fb7e5070.js"
+  },
+  {
+    "revision": "a3453de7f41f345da36326ad3ebd75bb",
+    "url": "/color-wheel/index.html"
+  }
+];
