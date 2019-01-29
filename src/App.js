24594920e58751a72527b062495a90622579ef8a
David Rose <doppler@gmail.com>
2019-01-29 18:21:47 -0500

    pretty much what I was after

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.scss";
 
-class App extends Component {
-  render() {
-    return <div className="App" />;
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <ColorWheel />
+    </div>
+  );
+};
 
 export default App;
+
+const ColorWheel = () => {
+  const div = 24;
+  return (
+    <div className="ColorWheel">
+      {Array.from(Array(div)).map((_, i) => (
+        <Radial key={i} rotation={(i * 360) / div} />
+      ))}
+    </div>
+  );
+};
+
+const Radial = ({ rotation }) => (
+  <div
+    className="Radial"
+    style={{
+      transform: `rotate(${rotation}deg)`,
+      backgroundColor: `hsl(${rotation}, 100%, 50%)`
+    }}
+  >
+    RADIAL
+  </div>
+);
