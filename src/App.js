15bf0c243460c8cf6caac4f63c3950ae30ee3d60
David Rose <doppler@gmail.com>
2019-01-29 18:47:02 -0500

    change README and add a link to github project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ const App = () => {
   return (
     <div className="App">
       <ColorWheel />
+      <a className="github-link" href="https://github.com/doppler/color-wheel">
+        View Source
+      </a>
     </div>
   );
 };
