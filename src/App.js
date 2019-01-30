0122c4e5f224ed4dbff92e77b6ecfe982fdf7322
David Rose <doppler@gmail.com>
2019-01-29 21:34:20 -0500

    add a slider to control number of radials

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 
 const App = () => {
+  const [numberOfRadials, setNumberOfRadials] = useState(25);
+
+  const handleRangeChange = event => {
+    console.log(event.target.value);
+    setNumberOfRadials(Number(event.target.value));
+  };
+
   return (
     <div className="App">
-      <ColorWheel />
+      <ColorWheel numberOfRadials={numberOfRadials} />
       <a className="github-link" href="https://github.com/doppler/color-wheel">
         View Source
       </a>
+      <RadialCountSlider
+        numberOfRadials={numberOfRadials}
+        handleRangeChange={handleRangeChange}
+      />
     </div>
   );
 };
 
 export default App;
 
-const ColorWheel = () => {
-  const div = 25;
+const ColorWheel = ({ numberOfRadials }) => {
   return (
     <div className="ColorWheel">
-      {Array.from(Array(div)).map((_, i) => (
-        <Radial key={i} rotation={(i * 360) / div} />
+      {Array.from(Array(numberOfRadials)).map((_, i) => (
+        <Radial key={i} rotation={(i * 360) / numberOfRadials} />
       ))}
     </div>
   );
@@ -32,5 +42,16 @@ const Radial = ({ rotation }) => (
       transform: `rotate(${rotation}deg)`,
       backgroundColor: `hsl(${rotation}, 100%, 50%)`
     }}
+  />
+);
+
+const RadialCountSlider = ({ numberOfRadials, handleRangeChange }) => (
+  <input
+    id="RadialCountSlider"
+    type="range"
+    value={numberOfRadials}
+    min={0}
+    max={360}
+    onChange={handleRangeChange}
   />
 );
